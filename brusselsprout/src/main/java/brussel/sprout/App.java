65d45a6473b61cd3cd8bc3970509545c17c3f20d
Kieran <e1156748@u.nus.edu>
2025-05-07 00:00:10 +0800

    Add maven-quickstart template

---
+++ brusselsprout/src/main/java/brussel/sprout/App.java
@@ -0,0 +1,13 @@
+package brussel.sprout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
